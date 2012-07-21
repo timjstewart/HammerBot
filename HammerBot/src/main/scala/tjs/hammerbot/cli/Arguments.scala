760e25d18b616cbf729e32985a2e15637cfab39f
Tim Stewart <tim.j.stewart@gmail.com>
2012-07-21 12:57:09 -0600

    Setting up multiple projects

---
+++ HammerBot/src/main/scala/tjs/hammerbot/cli/Arguments.scala
@@ -0,0 +1,59 @@
+package tjs.hammerbot.cli
+
+object Arguments {
+
+  def parse(args: Array[String]): Either[String, Arguments] = args.toList match {
+    case arg :: rest => parseCommand(arg, rest, Arguments.empty)
+    case _           => Right(Arguments.empty) 
+  }
+
+  private def parseCommand(
+    command:      String, 
+    commandFlags: List[String], 
+    result:       Arguments
+  ): Either[String, Arguments] = {
+    parseFlags(commandFlags, result.withCommand(command))
+  }  
+
+  private def parseFlags(
+    commandFlags: List[String], 
+    result:       Arguments
+  ): Either[String, Arguments] = commandFlags match {
+    case flag :: rest => isFlag(flag) match {
+      case true  => parseFlag(flag, rest, result)
+      case false => parseNames(commandFlags, result)
+    }
+    case _ => Right(result)
+  }
+
+  private def isFlag(s: String): Boolean = s.startsWith("-")
+
+  private def parseFlag(
+    flag:         String,
+    commandFlags: List[String], 
+    result:       Arguments
+  ): Either[String, Arguments] = flag match {
+    case "-d" => parseFlags(commandFlags, result.withDebug)
+    case _    => Left("Unrecognized flag: %s".format(flag))
+  }
+
+  private def parseNames(
+    names:   List[String], 
+    result:  Arguments
+  ): Either[String, Arguments] = names match {
+    case name :: rest => parseNames(rest, result.withName(name))
+    case _            => Right(result)
+  }
+
+  private def empty = Arguments("help", false, List())
+}
+
+case class Arguments(
+  val command: String,
+  val debug: Boolean,
+  val names: List[String]
+) {
+  def withDebug = Arguments(command, true, names)
+  def withCommand(command: String) = Arguments(command, debug, names)
+  def withName(name: String) = Arguments(command, debug, names ::: List(name))
+}
---
+++ HammerBot/src/main/scala/tjs/hammerbot/cli/CommandLineProcessor.scala
@@ -0,0 +1,9 @@
+package tjs.hammerbot.cli
+
+import tjs.hammerbot.model._
+
+object CommandLineProcessor {
+  def run(args: Array[String], suites: SuiteGroup): Unit = {
+    println("Hi")
+  }
+}
