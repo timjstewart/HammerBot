791894d272b3b9c13618eda701449a6b111dfb69
Tim Stewart <tim.j.stewart@gmail.com>
2012-07-20 21:56:30 -0600

    more code formatting

--- src/main/scala/tests/Tags.scala
+++ src/main/scala/tests/Tags.scala
@@ -4,15 +4,18 @@ import tjs.hammerbot._
 
 object Tags {
 
-  def getSuite() = suite("Tags", 
+  def getSuite() = suite("Tags Tests", 
 
    test("Google Test",
      get("https://mail.google.com/mail")
       .bodyContains("SetGmailCookie")),
 
+
    test("Delay Test",
      get("http://${blogHost}/blogs/delay")
-       .timeOut(600)),
+       .timeOut(600)
+       .saveBody("foo")),
+
 
    test("Get Tags",
      get("http://${blogHost}/blogs")
